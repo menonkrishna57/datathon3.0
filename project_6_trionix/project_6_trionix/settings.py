6f4da7596283e2f78a07882d7062ccd2ffcb04c9
Krishna Menon <66489697+menonkrishna57@users.noreply.github.com>
2025-01-28 20:37:58 +0530

    Integrated the login page to django

--- project_6_trionix/project_6_trionix/settings.py
+++ project_6_trionix/project_6_trionix/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR]
+STATIC_ROOT=BASE_DIR/'static'
+STATICFILES_DIRS=[BASE_DIR,'project_6_trionix/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
