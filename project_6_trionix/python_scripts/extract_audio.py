c2ab28f65befb68278b3343edfc172cfffd06023
Krishna Menon <66489697+menonkrishna57@users.noreply.github.com>
2025-02-01 17:01:59 +0530

    Refactor audio extraction and download functionality; remove youtube_downv2.py and add youtube_downv3.py for improved audio handling and create audio_down.html for user interface

--- project_6_trionix/python_scripts/extract_audio.py
+++ project_6_trionix/python_scripts/extract_audio.py
@@ -1,10 +1,12 @@
-import youtube_downv2 as ytd
 import assemblyai as aai
-
-aai.settings.api_key = "ce8209cdfb214d80b63881d941a2b015"
-transcriber = aai.Transcriber()
-
-transcript = transcriber.transcribe("data/aiml.mp4")
-
-with open("transcript.txt", "w") as file:
-    file.write(transcript.text)
+import os
+def main(filelink):    
+    aai.settings.api_key = "ce8209cdfb214d80b63881d941a2b015"
+    transcriber = aai.Transcriber()
+    file_path=os.path.join(os.getcwd(),"data","processed")
+    os.makedirs(file_path, exist_ok=True)
+    transcript = transcriber.transcribe(filelink)
+    filelink=os.path.join(file_path,"transcript.txt")
+    with open(filelink, "w") as file:
+        file.write(transcript.text)
+    return filelink
---
+++ project_6_trionix/python_scripts/youtube_downv3.py
@@ -0,0 +1,31 @@
+from pytubefix import YouTube
+import os
+# TODO: add audio download
+def download_youtube_audio(link):
+    yt = YouTube(link, 'WEB')
+    print(yt.title)
+    mypath=os.path.join(os.getcwd(),"project_6_trionix","data")
+    audiopath=os.path.join(mypath,f"{yt.title}.mp3")
+    ys = yt.streams.filter(only_audio=True).first()
+    if not os.path.exists(audiopath) or (os.path.exists(audiopath) and os.path.getsize(audiopath) != ys.filesize):
+        print("Downloading audio...")
+        ys.download(mypath, filename=f"{yt.title}.mp3")
+        print("Download completed!")
+    else:
+        print("File already exists!")
+    return os.path.join(audiopath)
+def download_youtube_video(link):
+    yt = YouTube(link, 'WEB')
+    print(yt.title)
+    mypath=os.path.join(os.getcwd(),"project_6_trionix","data")
+    vidpath=os.path.join(mypath,f"{yt.title}.mp4")
+    ys = yt.streams.get_highest_resolution()
+    if not os.path.exists(vidpath) or (os.path.exists(vidpath) and os.path.getsize(vidpath) != ys.filesize):
+        print("Downloading...")
+        ys.download(mypath)
+        print("Download completed!")
+    else:
+        print("File already exists!")
+    return str(os.path.join(vidpath))
+
+# print(download_youtube_video("https://youtu.be/9ngnCrBKWZA?si=rEuk5WUIk-csKFNm"))
